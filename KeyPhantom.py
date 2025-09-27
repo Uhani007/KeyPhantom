d8c4f605ec9c43ce44a27cf81fdd47fb792ef6c7
Umm-e-Hani <hani760076@gmail.com>
2025-09-27 20:58:56 +0500

    KeyPhantom.py

--- KeyPhantom.py
+++ KeyPhantom.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # Replace the IP with your server's IP for LAN usage
 # Example: SERVER_ADDRESS = '192.168.1.100:5555'
-SERVER_ADDRESS = '192.168.0.42:5555'
+SERVER_ADDRESS = '___________:5555'
 
 class KeyPhantom:
     def __init__(self):
